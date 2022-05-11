1832a03cffc493d2b984442edc1238b8b6178361
Joel Cormack <dgowellkomrak@gmail.com>
2022-05-11 11:21:52 -0500

    Add bulma navigation menu

--- functions.php
+++ functions.php
@@ -47,9 +47,10 @@ function air_head_sweden_setup() {
     add_theme_support( 'post-thumbnails' );
 
     // This theme uses wp_nav_menu() in one location.
+    require_once('navwalker.php');
     register_nav_menus(
         array(
-            'menu-1' => esc_html__( 'Primary', 'air-head-sweden' ),
+            'primary' => esc_html__( 'Primary', 'primary' ),
         )
     );
 
@@ -143,6 +144,8 @@ function air_head_sweden_scripts() {
     wp_style_add_data( 'air-head-sweden-style', 'rtl', 'replace' );
 
     wp_enqueue_script( 'air-head-sweden-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'air-head-sweden-bulma-navigation', get_template_directory_uri() . '/js/bulma-navigation.js', array(),
+    _S_VERSION, true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
