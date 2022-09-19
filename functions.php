a0a34951d0af90b592dfbfd711375be5e6eb7f1d
Joel Cormack <dgowellkomrak@gmail.com>
2022-09-19 12:25:32 +0100

    Add carousel

--- functions.php
+++ functions.php
@@ -144,16 +144,20 @@ add_action( 'widgets_init', 'air_head_sweden_widgets_init' );
 function air_head_sweden_scripts() {
     wp_enqueue_style( 'air-head-sweden-style', get_stylesheet_uri(), array(), _S_VERSION );
     wp_enqueue_style( 'bulma-style', get_template_directory_uri() . '/css/bulma.css', array(), _S_VERSION );
+    wp_enqueue_style( 'bulma-carousel', get_template_directory_uri() . '/css/bulma-carousel.min.css', array(), _S_VERSION );
     wp_enqueue_style( 'bulma-timeline', get_template_directory_uri() . '/css/bulma-timeline.min.css', array(), _S_VERSION );
     wp_style_add_data( 'air-head-sweden-style', 'rtl', 'replace' );
 
     wp_enqueue_script( 'air-head-sweden-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
     wp_enqueue_script( 'air-head-sweden-bulma-navigation', get_template_directory_uri() . '/js/bulma-navigation.js', array(),
     _S_VERSION, true );
+    wp_enqueue_script( 'air-head-sweden-bulma-carousel', get_template_directory_uri() . '/js/bulma-carousel.min.js',
+    array(), _S_VERSION, true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
     }
+
 }
 add_action( 'wp_enqueue_scripts', 'air_head_sweden_scripts' );
 
@@ -183,6 +187,11 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
     require get_template_directory() . '/inc/jetpack.php';
 }
+/**
+ * Load mobile detection Library
+ */
+require_once "libs/Mobile_Detect.php";
+
 /*
 * CUSTOM GLOBAL VARIABLES
 */
--- template-parts/section-product.php
+++ template-parts/section-product.php
@@ -6,17 +6,15 @@
  *
  * @package Air_Head_Sweden
  */
-$post_id = get_the_ID();
-$low = 'product-image-low';
-$high = 'product-image-high';
-
-?>
-<section id="homepage-banner" class="section is-desktop is-vcentered">
+//require_once "libs/Mobile_Detect.php";
+$detect = new Mobile_Detect;
+if ($detect->isMobile() || $detect->isTablet()) { ?>
+<section id="homepage-banner-mob" class="section is-vcentered">
     <div class="container">
         <div class="columns">
-            <div class="column is-5-desktop is-offset-7-desktop has-text-centered has-text-right-desktop">
-                <h1 class="title is-size-2 has-text-white">Air Head komposteringstoalett</h1>
-                <h2 class="subtitle is-size-4 has-text-white">
+            <div class="column has-text-centered has-text-right-desktop">
+                <h1 class="title has-text-white">Air Head komposteringstoalett</h1>
+                <h2 class="subtitle has-text-white">
                     Miljövänlig och offgrid separerings- och komposteringstoalett för små utrymmen, såsom båtar, bilar,
                     husvagnar, fritidshus
                 </h2>
@@ -26,3 +24,39 @@ $high = 'product-image-high';
         </div>
     </div>
 </section>
+<?php } else  { ?>
+<!-- Start Hero Carousel -->
+<section class="hero is-medium has-carousel">
+    <div id="homepage-carousel" class="hero-carousel">
+        <div class="item-1">
+            <img class="slide-img"
+                src="http://air-head-sweden-shop.local/wp-content/uploads/2022/09/catamarang-1-scaled.jpg">
+        </div>
+        <div class="item-2">
+            <img src="http://air-head-sweden-shop.local/wp-content/uploads/2022/09/van-in-snow.jpg" />
+        </div>
+        <div class="item-3">
+            <img src="http://air-head-sweden-shop.local/wp-content/uploads/2022/09/house-in-the-forest-scaled.jpg" />
+
+        </div>
+    </div>
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-5-desktop is-offset-7-desktop has-text-centered has-text-right-desktop py-6">
+                    <h1 class="title is-size-2 has-text-white">Air Head komposteringstoalett</h1>
+                    <h2 class="subtitle is-size-4 has-text-white">
+                        Miljövänlig och offgrid separerings- och komposteringstoalett för små utrymmen, såsom
+                        båtar, bilar,
+                        husvagnar, fritidshus
+                    </h2>
+                    <div class="hero-foot"><a href="/hur-fungerar-det">
+                            <button class="button is-white has-outline">Mer info</button>
+                        </a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Carousel -->
+<?php } //end else ?>
