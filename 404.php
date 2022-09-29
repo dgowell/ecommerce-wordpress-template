33bb7c134be2f2efe2ff2f7b1737414b395fa014
Joel Cormack <dgowellkomrak@gmail.com>
2022-09-29 14:44:10 +0100

    add mailchimp to 404

--- 404.php
+++ 404.php
@@ -10,51 +10,24 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'air-head-sweden' ); ?></h1>
-			</header><!-- .page-header -->
+    <section class="section error-404 not-found has-text-centered">
+        <header class="page-header">
+            <h1 class="page-title"><?php esc_html_e( 'Den sidan kan inte hittas.', 'air-head-sweden' ); ?>
+            </h1>
+        </header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'air-head-sweden' ); ?></p>
+        <div class="page-content">
+            <p><?php esc_html_e( 'Det ser ut som om ingenting hittades på denna plats.', 'air-head-sweden' ); ?>
+            </p>
 
-					<?php
-					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'air-head-sweden' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
+        </div><!-- .page-content -->
+    </section><!-- .error-404 -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$air_head_sweden_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'air-head-sweden' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$air_head_sweden_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-
+</main><!-- #main -->
+<?php get_template_part( 'template-parts/section-mailchimp', get_post_type() ); ?>
 <?php
 get_footer();
