fedb6547aef1751cc4ad57f2ea3c0cea6411bd49
Joel Cormack <dgowellkomrak@gmail.com>
2022-09-29 13:53:31 +0100

    Add Carousel CSS only version and remove bulma extension

--- functions.php
+++ functions.php
@@ -144,15 +144,12 @@ add_action( 'widgets_init', 'air_head_sweden_widgets_init' );
 function air_head_sweden_scripts() {
     wp_enqueue_style( 'air-head-sweden-style', get_stylesheet_uri(), array(), _S_VERSION );
     wp_enqueue_style( 'bulma-style', get_template_directory_uri() . '/css/bulma.css', array(), _S_VERSION );
-    wp_enqueue_style( 'bulma-carousel', get_template_directory_uri() . '/css/bulma-carousel.min.css', array(), _S_VERSION );
     wp_enqueue_style( 'bulma-timeline', get_template_directory_uri() . '/css/bulma-timeline.min.css', array(), _S_VERSION );
     wp_style_add_data( 'air-head-sweden-style', 'rtl', 'replace' );
 
     wp_enqueue_script( 'air-head-sweden-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
     wp_enqueue_script( 'air-head-sweden-bulma-navigation', get_template_directory_uri() . '/js/bulma-navigation.js', array(),
     _S_VERSION, true );
-    wp_enqueue_script( 'air-head-sweden-bulma-carousel', get_template_directory_uri() . '/js/bulma-carousel.min.js',
-    array(), _S_VERSION, true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
--- template-parts/section-product.php
+++ template-parts/section-product.php
@@ -10,43 +10,58 @@
 $detect = new Mobile_Detect;
 $base = get_site_url(null, '/wp-content/uploads/2022/09/', 'https');
 ?>
-<!-- Start Hero Carousel -->
-<section class="hero is-medium has-carousel">
-    <div id="homepage-carousel" class="hero-carousel">
-        <div class="item-1">
-            <?php if ($detect->isMobile()) { ?>
-            <img class="slide-img" src="<?php echo $base . "/boat-dark.jpg" ?>">
-            <?php } else { ?>
-            <img class="slide-img" src="<?php echo $base . "/catamarang-1-scaled-1.jpg" ?>">
-            <?php } ?>
-        </div>
-        <div class="item-2">
-            <?php if ($detect->isMobile()) { ?>
-            <img class="slide-img" src="<?php echo $base . "/forest-dark-2.jpg" ?>">
-            <?php } else { ?>
-            <img src="<?php echo $base . "/van-in-snow.jpg" ?>" />
-            <?php } ?>
-        </div>
-        <div class="item-3">
-            <?php if ($detect->isMobile()) { ?>
-            <img class="slide-img" src="<?php echo $base . "/van-dark-2.jpg" ?>">
-            <?php } else { ?>
-            <img src="<?php echo $base . "/house-in-the-forest-scaled-1.jpg" ?>" />
-            <?php } ?>
-        </div>
-    </div>
-    <div class="container homepage-banner">
-        <div class="column is-5-desktop is-offset-7-desktop has-text-centered has-text-right-desktop py-6">
-            <h1 class="title has-text-white">Air Head komposteringstoalett</h1>
-            <h2 class="subtitle has-text-white">
-                Miljövänlig och offgrid separerings- och komposteringstoalett för små utrymmen, såsom
-                båtar, bilar,
-                husvagnar, fritidshus
-            </h2>
-            <div class="hero-foot"><a href="/hur-fungerar-det">
-                    <button class="button is-white has-outline">Mer info</button>
-                </a></div>
-        </div>
-    </div>
+<section class="carousel" aria-label="Gallery">
+    <ol class="carousel__viewport">
+        <li id="carousel__slide1" tabindex="0" class="carousel__slide">
+            <div class="carousel__snapper">
+                <div class="container homepage-banner">
+                    <div class="column is-5-desktop is-offset-7-desktop has-text-centered has-text-right-desktop py-6">
+                        <h1 class="title has-text-white">Air Head komposteringstoalett</h1>
+                        <h2 class="subtitle has-text-white">
+                            Miljövänlig och offgrid separerings- och komposteringstoalett för små utrymmen, såsom
+                            båtar, bilar,
+                            husvagnar, fritidshus
+                        </h2>
+                        <div class="hero-foot"><a href="/hur-fungerar-det">
+                                <button class="button is-white has-outline">Mer info</button>
+                            </a></div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li id="carousel__slide2" tabindex="0" class="carousel__slide">
+            <div class="carousel__snapper">
+                <div class="container homepage-banner">
+                    <div class="column is-5-desktop is-offset-7-desktop has-text-centered has-text-right-desktop py-6">
+                        <h1 class="title has-text-white">Air Head komposteringstoalett</h1>
+                        <h2 class="subtitle has-text-white">
+                            Miljövänlig och offgrid separerings- och komposteringstoalett för små utrymmen, såsom
+                            båtar, bilar,
+                            husvagnar, fritidshus
+                        </h2>
+                        <div class="hero-foot"><a href="/hur-fungerar-det">
+                                <button class="button is-white has-outline">Mer info</button>
+                            </a></div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li id="carousel__slide3" tabindex="0" class="carousel__slide">
+            <div class="carousel__snapper">
+                <div class="container homepage-banner">
+                    <div class="column is-5-desktop is-offset-7-desktop has-text-centered has-text-right-desktop py-6">
+                        <h1 class="title has-text-white">Air Head komposteringstoalett</h1>
+                        <h2 class="subtitle has-text-white">
+                            Miljövänlig och offgrid separerings- och komposteringstoalett för små utrymmen, såsom
+                            båtar, bilar,
+                            husvagnar, fritidshus
+                        </h2>
+                        <div class="hero-foot"><a href="/hur-fungerar-det">
+                                <button class="button is-white has-outline">Mer info</button>
+                            </a></div>
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ol>
 </section>
-<!-- End Hero Carousel -->
